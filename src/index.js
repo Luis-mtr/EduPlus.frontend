f5a928f4446fce8410360c2ae1e037d75cb100d8
Nem090 <102314405+Nem090@users.noreply.github.com>
2024-06-24 02:06:30 +0300

    deployment updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <AuthProvider>
     <App />
-  </AuthProvider>,
-  document.getElementById("root")
+  </AuthProvider>
 );
